7bf9e5a31cad976462b32124ee7ed0fb3d4f1e75
bobrik <1337@ya.ru>
2023-07-17 12:01:03 +0700

    project1

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    type_of_log = models.CharField(max_length=255, verbose_name="type")
+    userdomain = models.CharField(max_length=255, verbose_name="userdomain")
+    hostname = models.CharField(max_length=255, verbose_name="hostname")
+    ipaddress = models.CharField(max_length=255, verbose_name="ip address")
+    type_of_service = models.CharField(max_length=255, verbose_name="Тип Входа в систему")
+    time_login = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')
+
+
+class AlienUser(models.Model):
+    type_of_log = models.CharField(max_length=255, verbose_name="type")
+    userdomain = models.CharField(max_length=255, verbose_name="userdomain")
+    hostname = models.CharField(max_length=255, verbose_name="hostname")
+    ipaddress = models.CharField(max_length=255, verbose_name="ip address")
+    type_of_service = models.CharField(max_length=255, verbose_name="Тип Входа в систему")
+    time_login = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')
