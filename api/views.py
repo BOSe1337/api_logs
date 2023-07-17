c71d8db8bb806d024c6b010680c27848323413aa
bobrik <1337@ya.ru>
2023-07-17 19:39:22 +0700

    add post

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import User
+
 
 # Create your views here.
 class UserAccessView(APIView):
@@ -14,5 +16,15 @@ class UserAccessView(APIView):
         return ip
 
     def post(self, request, *args, **kwargs):
+        if self.get_client_ip(request) == request.POST["ipaddress"]:
+            User.objects.create_user(
+                username=request.POST["username"],
+                type_of_log=request.POST["type"],
+                userdomain=request.POST["userdomain"],
+                hostname=request.POST["hostname"],
+                ipaddress=request.POST["ipaddress"],
+                type_of_service=request.POST["logontype"],
+            )
+            return Response("Insert done")
         # return Response(request.META["REMOTE_ADDR"])
         return Response(self.get_client_ip(request))
